597265106d5d30dec65ee1cf49464b2cc6343cba
saiseikyuu <saiseikyuu@gmail.com>
2025-07-21 18:51:17 +0800

    update: mapping

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hello React!</h1>
+const fruits = ['Apple', 'Banana', 'Cherry'];
+
+const listItems = fruits.map((fruit) => 
+  <li>{fruit}</li>
+)
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(listItems);
