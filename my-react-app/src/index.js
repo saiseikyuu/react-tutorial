446b210d6c2d73cb0e9f8874187f836c88f023c4
saiseikyuu <saiseikyuu@gmail.com>
2025-07-22 12:25:38 +0800

    Update: useState onChange

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Form(){
+  const [name, setName] = useState('');
 
   return(
     <form>
       <label>name
-      <input type="text" placeholder="Enter your name" />
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter your name" />
       </label>
     </form>
   )
-}
+} 
 
 
 
