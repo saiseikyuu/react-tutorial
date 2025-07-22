1879209d7e20a2145ae0a540fa492513055c23cf
saiseikyuu <saiseikyuu@gmail.com>
2025-07-22 12:30:00 +0800

    Update: React useState onSubmit

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 
 function Form(){
   const [name, setName] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`A name submitter: ${name}`)
+  }
+
 
   return(
     <form>
       <label>name
       <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter your name" />
       </label>
+      <input type="submit" value="Submit" onClick={handleSubmit} />
     </form>
   )
 } 
