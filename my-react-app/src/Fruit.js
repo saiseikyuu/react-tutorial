55b91b85bac6c46df561166b07ef3cf51cf845eb
saiseikyuu <saiseikyuu@gmail.com>
2025-07-28 23:09:00 +0800

    Update

--- my-react-app/src/Fruit.js
+++ my-react-app/src/Fruit.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
+
 function Shanghai() {
+  const [bgClass, setBgClass] = useState("bg-red-500");
 
-    const [color, setColor] = useState("red");
-    return (
-
-        <>
-            <h1>Current Color is {color}</h1>
-            <button onClick={() => setColor("Blue")}>Blue</button>
-            <button onClick={() => setColor("Red")}>Red</button>
-            <button onClick={() => setColor("Green")}>Green</button>
-        </>
-    )
+  return (
+    <div className={`${bgClass} h-screen flex flex-col items-center justify-center gap-6`}>
+      <h1 className="text-white text-2xl">This is a {bgClass} background</h1>
+      <div className="flex gap-4">
+        <button onClick={() => setBgClass("bg-blue")} className="bg-white text-blue-500 px-4 py-2 rounded">Blue</button>
+        <button onClick={() => setBgClass("bg-red-500")} className="bg-white text-red-500 px-4 py-2 rounded">Red</button>
+        <button onClick={() => setBgClass("bg-green-500")} className="bg-white text-green-500 px-4 py-2 rounded">Green</button>
+      </div>
+    </div>
+  );
 }
 
 export default Shanghai;
