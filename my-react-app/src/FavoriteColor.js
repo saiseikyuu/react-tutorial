2ada4c2d60f3a6ee7f34771743ee3979b3ca3f27
saiseikyuu <saiseikyuu@gmail.com>
2025-07-29 22:38:38 +0800

    Update

--- my-react-app/src/FavoriteColor.js
+++ my-react-app/src/FavoriteColor.js
@@ -1,19 +1,12 @@
 import React, { useState } from "react";
 
 function FavoriteColor() {
-  const [bgClass, setBgClass] = useState("bg-red-500");
+  const [color, setColor] = useState("");
 
   return (
-    <div className={`${bgClass} h-screen flex flex-col items-center justify-center gap-6`}>
-      <h1 className="text-white text-2xl">This is a {bgClass} background</h1>
-      <div className="flex gap-4">
-        <button onClick={() => setBgClass("Blue")} className="bg-white text-blue-500 px-4 py-2 rounded">Blue</button>
-        <button onClick={() => setBgClass("Red")} className="bg-white text-red-500 px-4 py-2 rounded">Red</button>
-        <button onClick={() => setBgClass("Green")} className="bg-white text-green-500 px-4 py-2 rounded">Green</button>
-        <button onClick={() => setBgClass("Pink")} className="bg-white text-green-500 px-4 py-2 rounded">Pink</button>
-        <button onClick={() => setBgClass("Violet")} className="bg-white text-green-500 px-4 py-2 rounded">Violet</button>
-      </div>
-    </div>
+ <>
+ 
+ </>
   );
 }
 
