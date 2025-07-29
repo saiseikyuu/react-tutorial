3916a2fbe5c5a1319730d276a135b47722354953
saiseikyuu <saiseikyuu@gmail.com>
2025-07-29 22:58:48 +0800

    Update

--- my-react-app/src/FavoriteColor.js
+++ my-react-app/src/FavoriteColor.js
@@ -8,12 +8,20 @@ function FavoriteColor() {
     color: "blue"
   });
   
+  const updateColor = () => {
+    setCar(prevCar => {
+      return {
+        ...prevCar,
+        color: prevCar.color === "blue" ? "red" : "blue"
+      };
+    })
+  }
 
   return (
  <>
   <h1>My {car.brand}</h1>
   <p>It is a {car.color} {car.model} from {car.year}</p>
-  <button onClick={() => setCar({color:"red"})}>Change color</button>
+  <button onClick={updateColor}>Change color</button>
  </>
   );
 }
