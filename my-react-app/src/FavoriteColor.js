ff0eb119637c4f7a10f1278b61cb3f556647bf42
saiseikyuu <saiseikyuu@gmail.com>
2025-07-29 22:42:52 +0800

    Update

--- my-react-app/src/FavoriteColor.js
+++ my-react-app/src/FavoriteColor.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 
 function FavoriteColor() {
-  const [color, setColor] = useState("");
+  const [color, setColor] = useState("No Favorite Color");
 
   return (
  <>
-  <h1>My Favorite color is {color}</h1>
+  <h1>{color}</h1>
+  <button onClick={() => setColor("My Favorite Color is Red")}>Set Color to Red</button>
  </>
   );
 }
