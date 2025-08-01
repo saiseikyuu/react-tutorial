1ba7bac1e82d9e5d01000447df1fdedafcbb9547
saiseikyuu <saiseikyuu@gmail.com>
2025-08-01 22:44:37 +0800

    Update

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -2,3 +2,14 @@ import { useState, createContext } from "react";
 import ReactDOM from "react-dom/client";
 
 const UserContext = createContext()
+
+function Component1() {
+  const [user, setUser] = useState("Jesse Hall");
+
+  return (
+    <UserContext.Provider value={user}>
+      <h1>{`Hello ${user}!`}</h1>
+      <Component2 user={user} />
+    </UserContext.Provider>
+  );
+}
