115e42a7329e2c3ec7bd6d3818e7b80227b18f09
saiseikyuu <saiseikyuu@gmail.com>
2025-08-01 22:56:00 +0800

    Update: useRef

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,58 +1,25 @@
-import { useState, createContext, useContext } from "react";
+import { useState, useEffect,useRef } from "react";
 import ReactDOM from "react-dom/client";
 
-const UserContext = createContext();
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
 
-function Component1() {
-  const [user, setUser] = useState("Jesse Hall");
-
-  return (
-    <UserContext.Provider value={user}>
-      <h1>{`Hello ${user}!`}</h1>
-      <Component2 />
-    </UserContext.Provider>
-  );
-}
-
-function Component2() {
-  return (
-    <>
-      <h1>Component 2</h1>
-      <Component3 />
-    </>
-  );
-}
-
-function Component3() {
-  return (
-    <>
-      <h1>Component 3</h1>
-      <Component4 />
-    </>
-  );
-}
-
-function Component4() {
-  return (
-    <>
-      <h1>Component 4</h1>
-      <Component5 />
-    </>
-  );
-}
-
-function Component5() {
-  const user = useContext(UserContext);
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
 
   return (
     <>
-      <h1>Component 5</h1>
-      <h2>{`Hello ${user} again!`}</h2>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h1>Render Count: {count.current}</h1>
     </>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Component1 />);
-
-              
+root.render(<App />);
