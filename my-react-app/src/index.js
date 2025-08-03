75de13fe5dfe31a4894bc1aa6a585dc59cc78a9f
saiseikyuu <saiseikyuu@gmail.com>
2025-08-03 12:47:26 +0800

    Update

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,28 +1,44 @@
-import { useState, useCallback } from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom/client";
-import Todos from "./Todos";
 
 const App = () => {
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState([]);
+  const calculation = expensiveCalculation(count);
 
   const increment = () => {
     setCount((c) => c + 1);
   };
-  const addTodo = useCallback(() => {
+  const addTodo = () => {
     setTodos((t) => [...t, "New Todo"]);
-  }, [todos]);
+  };
 
   return (
-    <>
-      <Todos todos={todos} addTodo={addTodo} />
+    <div>
+      <div>
+        <h2>My Todos</h2>
+        {todos.map((todo, index) => {
+          return <p key={index}>{todo}</p>;
+        })}
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
       <hr />
       <div>
         Count: {count}
         <button onClick={increment}>+</button>
+        <h2>Expensive Calculation</h2>
+        {calculation}
       </div>
-    </>
+    </div>
   );
+};
+
+const expensiveCalculation = (num) => {
+  console.log("Calculating...");
+  for (let i = 0; i < 1000000000; i++) {
+    num += 1;
+  }
+  return num;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
