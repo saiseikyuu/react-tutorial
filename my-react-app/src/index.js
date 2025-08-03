43dc5005ed514914f509f257715f0e85367db74a
saiseikyuu <saiseikyuu@gmail.com>
2025-08-03 12:11:46 +0800

    Update

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,58 +1,29 @@
-import { useReducer } from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom/client";
+import Todos from "./Todos";
 
-const initialTodos = [
-  {
-    id: 1,
-    title: "Todo 1",
-    complete: false,
-  },
-  {
-    id: 2,
-    title: "Todo 2",
-    complete: false,
-  },
-];
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "COMPLETE":
-      return state.map((todo) => {
-        if (todo.id === action.id) {
-          return { ...todo, complete: !todo.complete };
-        } else {
-          return todo;
-        }
-      });
-    default:
-      return state;
-  }
-};
-
-function Todos() {
-  const [todos, dispatch] = useReducer(reducer, initialTodos);
-
-  const handleComplete = (todo) => {
-    dispatch({ type: "COMPLETE", id: todo.id });
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"]);
   };
 
   return (
     <>
-      {todos.map((todo) => (
-        <div key={todo.id}>
-          <label>
-            <input
-              type="checkbox"
-              checked={todo.complete}
-              onChange={() => handleComplete(todo)}
-            />
-            {todo.title}
-          </label>
-        </div>
-      ))}
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
     </>
   );
-}
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Todos />);
+root.render(<App />);
