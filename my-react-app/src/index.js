fc033fb767b6becd0437c437ca9acc2e2bfb79e1
saiseikyuu <saiseikyuu@gmail.com>
2025-08-03 12:48:25 +0800

    Update

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import ReactDOM from "react-dom/client";
 
 const App = () => {
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState([]);
-  const calculation = expensiveCalculation(count);
+  const calculation = useMemo(() => expensiveCalculation(count), [count]);
 
   const increment = () => {
     setCount((c) => c + 1);
